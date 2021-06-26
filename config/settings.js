6872b95e4e504eb9585eb5a479dc86b96f030507
Caleb Adewole <caleberioluwa@gmail.com>
2021-06-26 02:14:35 +0100

    add project settings

---
+++ config/settings.js
@@ -0,0 +1,28 @@
+require("../lib/envFileReader")();
+const environment = {};
+environment.staging = {
+  port: parseInt(process.env.PORT),
+  env: process.env.NODE_ENV,
+  clientKey: process.env.CLIENT_KEY,
+  clientSecret: process.env.CLIENT_SECRET,
+};
+
+environment.production = {
+  port: parseInt(process.env.PRODUCTION_PORT),
+  env: process.env.NODE_ENV,
+  clientKey: process.env.PRODUCTION_CLIENT_KEY,
+  clientSecret: process.env.PRODUCTION_CLIENT_SECRET,
+};
+
+let currentEnv =
+  typeof process.env.NODE_ENV.toLowerCase() == "string" &&
+  typeof process.env.NODE_ENV != "undefined"
+    ? process.env.NODE_ENV.toLowerCase()
+    : "staging";
+
+// eslint-disable-next-line no-prototype-builtins
+let appEnvironment = environment.hasOwnProperty(currentEnv)
+  ? environment[currentEnv]
+  : environment.staging;
+
+module.exports = appEnvironment;
