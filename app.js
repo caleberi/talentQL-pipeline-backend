6b2172dc2d050271a8f72b069d0ea95e72de4f9b
Caleb Adewole <caleberioluwa@gmail.com>
2021-06-27 18:56:43 +0100

    application initialization

--- app.js
+++ app.js
@@ -1,3 +1,7 @@
-const config = require("./config/settings");
+const config = require("./config/settings.config");
+const Servify = require("./lib/servify.lib");
+const app = new Servify();
 
-console.log(config);
+app.listen(config.port, (port) => {
+  return () => console.log(`Server running at http://localhost:${port} `);
+});
