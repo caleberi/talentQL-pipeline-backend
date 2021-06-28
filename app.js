3a5fcbcdc95b8fcd3c629baf170a0f2144a7e3d1
Caleb Adewole <caleberioluwa@gmail.com>
2021-06-28 01:03:52 +0100

    test if server logic is getting better

--- app.js
+++ app.js
@@ -1,6 +1,15 @@
+const { request } = require("http");
 const config = require("./config/settings.config");
 const Servify = require("./lib/servify.lib");
 const app = new Servify();
+
+app
+  .get("/card", (request, response) => {
+    response.status(200).json(response.context);
+  })
+  .post("/card", (request, response) => {
+    response.status(200).json(request.context);
+  });
 
 app.listen(config.port, (port) => {
   return () => console.log(`Server running at http://localhost:${port} `);
