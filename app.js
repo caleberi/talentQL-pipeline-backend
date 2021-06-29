67198245bacbc2826fa0a212e35ea66146cf2d8c
Caleb Adewole <caleberioluwa@gmail.com>
2021-06-29 15:51:44 +0100

    summary for now

--- app.js
+++ app.js
@@ -5,10 +5,10 @@ const app = new Servify();
 
 app
   .get("/card", (request, response) => {
-    response.status(200).json(response.context);
+    response.status(200).json(request.context);
   })
   .post("/card/:id/book/:book", (request, response) => {
-    response.status(200).json(request.context);
+    response.status(200).xml(request.context);
   });
 
 app.listen(config.port, (port) => {
