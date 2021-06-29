42c90f06315ae1e21d516aeb1d3d1d41218e3855
Caleb Adewole <caleberioluwa@gmail.com>
2021-06-29 23:55:04 +0100

    fixed the resolver function for clean code

--- app.js
+++ app.js
@@ -1,15 +1,12 @@
 const { request } = require("http");
 const config = require("./config/settings.config");
 const Servify = require("./lib/servify.lib");
+const cardResolver = require("./resolvers/card.resolver");
 const app = new Servify();
 
-app
-  .get("/card", (request, response) => {
-    response.status(200).json(request.context);
-  })
-  .post("/card/:id/book/:book", (request, response) => {
-    response.status(200).xml(request.context);
-  });
+app.get("/ping", cardResolver.ping);
+app.get("/echo", cardResolver.echo);
+app.post("/card", cardResolver.validateCard);
 
 app.listen(config.port, (port) => {
   return () => console.log(`Server running at http://localhost:${port} `);
