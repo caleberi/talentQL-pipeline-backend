24d3e246074286e99d828144e9337e96b1647c15
Caleb Adewole <caleberioluwa@gmail.com>
2021-06-30 06:46:22 +0100

    update card.resolver.js

--- resolvers/card.resolver.js
+++ resolvers/card.resolver.js
@@ -1,12 +1,35 @@
+const {
+  CreditCardDetails,
+  CreditCardError,
+} = require("../model/creditCard.model");
 exports.echo = (request, response) => {
   response.status(200).json(request.context);
 };
 
 exports.validateCard = (request, response) => {
-  response.status(200).xml(request.context);
+  try {
+    let { body } = request.context;
+    let { cardNumber, cvv, mobileNos, phoneNumber, email, expiryDate } = body;
+    let { error, success } = new CreditCardDetails(
+      cardNumber,
+      expiryDate,
+      cvv,
+      email,
+      mobileNos,
+      phoneNumber
+    ).validate();
+    if (error) {
+      throw new CreditCardError(error, "Card Validation Error");
+    }
+    response.status(200).json(success);
+    return;
+  } catch (err) {
+    response.status(400).json(err);
+  }
 };
 
 exports.ping = (request, response) => {
+  let _ = request;
   response
     .status(200)
     .send("<div style='color:red;'>Hello World</div>", "text/html");
